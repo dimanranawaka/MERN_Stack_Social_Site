a1409b1183bdbf57793d82a088e339a88fc20cee
dimanranawaka <dimanranawaka@gmail.com>
2024-03-04 02:07:09 +0530

    creating userSchema

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const port = 5000;
 
 const {MONGOURI} = require('./keys');
 
+// register the model
+require('./models/user');
+
+// register the routes
 mongoose.connect(MONGOURI);
+
 mongoose.connection.on('connected',()=>{
     console.log("Connected to mongo");
 });
