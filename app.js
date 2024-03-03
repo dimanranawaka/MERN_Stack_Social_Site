40080f1e0b21622966cb5d5e07bdefaccceb59b0
dimanranawaka <dimanranawaka@gmail.com>
2024-03-03 15:34:05 +0530

    content updated

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ const port = 5000;
 
 const customMiddleware = (req, res, next) => {
   console.log("Middleware Executed");
+  next();
 }
 
+// Passing the middleware to the app
 app.use(customMiddleware);
 
 app.get('/', (req, res) => {
+  console.log("Home");
   res.send('Hello World');
 });
 
 app.listen(port, () => {
   console.log("Server is running on port ",port);
+});
+
+app.get('/about', (req, res) => {
+  console.log("About");
+  res.send('About Page');
 });
