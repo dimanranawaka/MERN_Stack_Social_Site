8ccd52fc8bcb0d07d68b5d5fdbe2628cee8016b2
dimanranawaka <dimanranawaka@gmail.com>
2024-03-05 17:20:41 +0530

    signup route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = mongoose.model("User");
 
 router.get('/',(req,res)=>{
     res.send("hello");
@@ -12,7 +15,28 @@ router.post('/signup',(req,res)=>{
     if(!email || !password || !name){
         return res.status(422).json({error:"Please add all the fields"});
     }
-    res.json({message:"Successfully posted"});
+    
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"User already exists with that email"});
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        });
+        user.save()
+        .then(user=>{
+            res.json({message:"Saved successfully"});
+        })
+        .catch(err=>{
+            console.log(err);
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+    });
 });
 
 module.exports = router;
