4d2a9cc748600e3bc3ecca54c4a3d0ec27122fa9
dimanranawaka <dimanranawaka@gmail.com>
2024-03-05 17:42:51 +0530

    hashing passwords

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
 
-router.get('/',(req,res)=>{
-    res.send("hello");
-});
+
 
 router.post('/signup',(req,res)=>{ 
 
@@ -21,18 +20,28 @@ router.post('/signup',(req,res)=>{
         if(savedUser){
             return res.status(422).json({error:"User already exists with that email"});
         }
-        const user = new User({
-            email,
-            password,
-            name
+
+        // hashing the password
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name
+            });
+
+            user.save()
+            .then(user=>{
+                res.json({message:"Saved successfully"});
+            })
+            .catch(err=>{
+                console.log(err);
+            });
+
         });
-        user.save()
-        .then(user=>{
-            res.json({message:"Saved successfully"});
-        })
-        .catch(err=>{
-            console.log(err);
-        });
+
+        
     })
     .catch(err=>{
         console.log(err);
