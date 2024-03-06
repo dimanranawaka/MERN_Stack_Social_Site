86e192b25acb830e91df78d05b2c0e3477fd69c1
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 21:31:51 +0530

    signIn Ui

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -3,14 +3,15 @@ import React from 'react';
 const Login = () => {
 
     return (
-        <div>
-            <div class="card"> </div>
-            <input type="text" placeholder="email" />
-            <input type="text" placeholder="password" />
+        <div className="mycard">
+            <div className="card auth-card">
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
 
-            <button class="btn waves-effect waves-light">
-                Login
-            </button>
+                <button class="btn waves-effect waves-light">
+                    Login
+                </button>
+            </div>
         </div>
     );
 };
