5ef6758a5ac004652a932b8b75b672d767fd3a1f
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 23:32:58 +0530

    profile Ui

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const Profile = () => {
-
     return (
-        <div>
+        <div style={{ maxWidth: "550px", margin: "0px auto" }}>
             <div style={{
                 display: "flex",
                 justifyContent: "space-around",
@@ -12,7 +11,7 @@ const Profile = () => {
             }}>
                 <div>
                     <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
-                    src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww"/>
+                        src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww" />
                 </div>
                 <div>
                     <h4>Diman Ranawaka</h4>
@@ -20,7 +19,6 @@ const Profile = () => {
                         display: "flex",
                         justifyContent: "space-between",
                         width: "108%"
-                    
                     }}>
                         <h5>50 posts</h5>
                         <h5>50 followers</h5>
@@ -28,9 +26,20 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
+
+            <div style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around"
+            }}>
+                <img style={{ width: "30%", margin: "1%" }} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww" />
+                <img style={{ width: "30%", margin: "1%" }} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww" />
+                <img style={{ width: "30%", margin: "1%" }} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww" />
+                <img style={{ width: "30%", margin: "1%" }} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww" />
+                <img style={{ width: "30%", margin: "1%" }} src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww" />
+            </div>
         </div>
     );
-
 };
 
 export default Profile;
