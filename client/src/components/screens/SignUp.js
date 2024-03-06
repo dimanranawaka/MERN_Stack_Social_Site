1fb840636098c0d10f7ac6cb785ce894266c97a2
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 21:51:54 +0530

    signUp Ui

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -3,7 +3,18 @@ import React from 'react';
 const SignUp = () => {
 
     return (
-        <h1>SignUp</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Login
+                </button>
+            </div>
+        </div>
     );
 };
 
