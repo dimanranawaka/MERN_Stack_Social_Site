042c587f7f0bf81b96bfd4a4206ccd7ea3c3bb08
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 22:39:26 +0530

    profile Ui

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,12 +4,21 @@ const Profile = () => {
 
     return (
         <div>
-            <div>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+            }}>
                 <div>
                     <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
-                    src="https://unsplash.com/photos/grayscale-photo-of-man-c_GmwfHBDzk"/>
+                    src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fHww"/>
                 </div>
-                <div></div>
+                <div>
+                    <h4>Diman Ranawaka</h4>
+                    <div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     );
