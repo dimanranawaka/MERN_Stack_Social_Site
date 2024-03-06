4bd17f30d2cce8ebba5a0c912556de248a0584ae
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 23:45:35 +0530

    home Ui

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                
+            </div>
+        </div>
     );
 };
 
