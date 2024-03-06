7d737e4969d676c40c2b9b47927e237857ee23e1
dimanranawaka <dimanranawaka@gmail.com>
2024-03-07 02:31:50 +0530

    CreatePost.js impl

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const CreatePost = () => {};
-
+const CreatePost = () => {
+    return (
+        <div className="card input-filed">
+            <input type="text" placeholder="title"/>
+            <input type="text" placeholder="body"/>
+            <div className="file-field input-field">
+                <div className="btn">
+                    <span>Upload Image</span>
+                    <input type="file"/>
+                </div>
+                <div className="file-path-wrapper">
+                    <input className="file-path validate" type="text"/>
+                </div>
+            </div>
+            <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+                Submit Post
+            </button>
+        </div>
+    );
+};
 
 export default CreatePost;
