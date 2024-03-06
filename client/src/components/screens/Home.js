1f27c99d8636d035717bf37c67a197bf47e93954
dimanranawaka <dimanranawaka@gmail.com>
2024-03-06 23:48:49 +0530

    home Ui

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -4,7 +4,10 @@ const Home = () => {
     return (
         <div className="home">
             <div className="card home-card">
-                
+                <h5>Diman</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8MHwwfHx8MA%3D%3D"/>
+                </div>
             </div>
         </div>
     );
