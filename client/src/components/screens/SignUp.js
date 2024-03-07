cc9d889d3c0b37c88b703010616fb4a25ce98d57
dimanranawaka <dimanranawaka@gmail.com>
2024-03-07 13:36:16 +0530

    implement for user signup

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React,{userState} from 'react';
 import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+
+    const [name,setName] = userState("");
+    const [password,setPassword] = userState("");
+    const [email,setEmail] = userState("");
+
+    const PostData = ()=>{
+        fetch("http:/localhost:5000/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name:"",
+                password:"",
+                email:""
+            })
+        }).then(res=>res.json())
+    }
 
     return (
         <div className="mycard">
             <div className="card auth-card input-field">
                 <h2>Dexter</h2>
-                <input type="text" placeholder="name" />
-                <input type="text" placeholder="email" />
-                <input type="text" placeholder="password" />
+                <input type="text" placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                <input type="text" placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                <input type="text" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
                     Sign Up
