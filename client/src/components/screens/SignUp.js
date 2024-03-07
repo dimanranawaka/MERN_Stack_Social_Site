3357b7a70b9d71d37b1af7ab4f3f92db000305ba
dimanranawaka <dimanranawaka@gmail.com>
2024-03-07 16:26:03 +0530

    implement for user signup

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -23,6 +23,8 @@ const SignUp = () => {
         .then(data=>{
             if(data.error){
                 M.toast({html: data.error, classes:"#c62828 red darken-3"})
+            }else{
+                M.toast({html:data.message, classes:"#43a047 green darken-1"})
             }
         })
     }
