a37c217cf0a716a31c7fba7dd3a0e43f21777af9
dimanranawaka <dimanranawaka@gmail.com>
2024-03-07 16:15:44 +0530

    Proxying API Requests

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -8,7 +8,7 @@ const SignUp = () => {
     const [email,setEmail] = useState("");
 
     const PostData = ()=>{
-        fetch("http://localhost:5000/signup",{
+        fetch("/signup",{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
