6bba376fdeb292d79c7b1ffede53476f6d71b25a
dimanranawaka <dimanranawaka@gmail.com>
2024-03-07 18:53:09 +0530

    email regular expression

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -9,6 +9,10 @@ const SignUp = () => {
     const [email,setEmail] = useState("");
 
     const PostData = ()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html: "Invalid email", classes:"#c62828 red darken-3"});
+            return;
+        }
         fetch("/signup",{
             method:"post",
             headers:{
