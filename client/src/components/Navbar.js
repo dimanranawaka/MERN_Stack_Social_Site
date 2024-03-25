be3f878bba8ed81c3c575ef1c2ad366b6ba0e3c1
dimanranawaka <dimanranawaka@gmail.com>
2024-03-25 17:34:20 +0530

    changing navigation links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
 
+// Define a functional component called Navbar
 const Navbar = () => {
+    // Use the useContext Hook to access the UserContext. This gives us access to the state and dispatch function from the UserContext.
+    const { state, dispatch } = useContext(UserContext);
+
+    // Define a function called renderList
+    const renderList = () => {
+        // Check if there is a state (i.e., if the user is logged in)
+        if (state) {
+            // If there is a state (user is logged in), return an array of list items with links to the Profile and Create Post pages
+            return [
+                <li><Link to="/Profile">Profile</Link></li>,
+                <li><Link to="/Create">Create Post</Link></li>
+            ]
+        } else {
+            // If there is no state (user is not logged in), return an array of list items with links to the SignIn and SignUp pages
+            return [
+                <li><Link to="/SignIn">SignIn</Link></li>,
+                <li><Link to="/SignUp">SignUp</Link></li>
+            ]
+        }
+    }
+
     return (
         <nav>
-            <div className="nav-wrapper white"> 
-                <Link to="/" className="brand-logo">Dexter</Link>
+            <div className="nav-wrapper white">
+                <Link to={state ? "/" : "signIn"} className="brand-logo">Dexter</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><Link to="/SignIn">SignIn</Link></li>
-                    <li><Link to="/SignUp">SignUp</Link></li>
-                    <li><Link to="/Profile">Profile</Link></li>
-                    <li><Link to="/Create">Create Post</Link></li>
+
+                    {renderList()}
+
                 </ul>
             </div>
         </nav>
