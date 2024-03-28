be09e146d47f913a16bb64449c441767bb988cbf
dimanranawaka <dimanranawaka@gmail.com>
2024-03-29 02:30:46 +0530

    blackend api for like and unlike

--- server/models/post.js
+++ server/models/post.js
@@ -1,27 +1,31 @@
 const mongoose = require('mongoose'); // Import mongoose
-const {ObjectId} = mongoose.Schema.Types; // Import ObjectId from mongoose
+const { ObjectId } = mongoose.Schema.Types; // Import ObjectId from mongoose
 
 // Create a schema
 
 const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    body:{
-        type:String,
-        required:true
+    body: {
+        type: String,
+        required: true
     },
-    photo:{
-        type:String,
-        required:true	
+    photo: {
+        type: String,
+        required: true
     },
-    postedBy:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+    likes: [{
+        type: ObjectId,
+        ref: "User"
+    }],
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
 });
 
 // Create a model
 
-mongoose.model("Post",postSchema); // Create a model with the name "Post" and the schema "postSchema"
+mongoose.model("Post", postSchema); // Create a model with the name "Post" and the schema "postSchema"
--- server/routes/post.js
+++ server/routes/post.js
@@ -50,4 +50,39 @@ router.get('/mypost', requireLogin, (req, res) => {
         });
 });
 
+// Like a post
+
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        // push the user id to the likes array
+        $push: { likes: req.user._id }
+    }, {
+        new: true
+    }).exec((err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+// Unlike a post
+
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        // pull the user id from the likes array
+        $pull: { likes: req.user._id }
+    }, {
+        new: true
+    }).exec((err, result) => {
+        if (err) {
+            return res.status(422).json({ error: err });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
 module.exports = router;
