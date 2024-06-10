ce97e2e14358c0f3e932b10e25d8008216b6c0f6
dimanranawaka <dimanranawaka@gmail.com>
2024-06-10 19:20:25 +0530

    Node API for follw and unfollow user

--- server/models/user.js
+++ server/models/user.js
@@ -13,8 +13,9 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
-
+    },
+    followers: [{ type: ObjectId, ref: "User" }],
+    following: [{ type: ObjectId, ref: "User" }]
 });
 
 // export the model
