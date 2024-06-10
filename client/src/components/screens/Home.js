8c70a15fc8a710b973c530002153387e4b23e5d1
dimanranawaka <dimanranawaka@gmail.com>
2024-06-10 15:58:44 +0530

    implement delete post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -98,6 +98,22 @@ const Home = () => {
             })
     }
 
+    const deletePost = (postid) => {
+        fetch(`/deletepost/${postid}`, {
+            method: "delete",
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                console.log(result);
+                const newData = data.filter(item => {
+                    return item._id !== result._id
+                })
+                setData(newData)
+            })
+    }
+
     return (
         <div className="home">{
 
@@ -106,7 +122,7 @@ const Home = () => {
 
                     <div className="card home-card" key={item._id}>
 
-                        <h5>{item.postedBy.name}</h5>
+                        <h5>{item.postedBy.name}{item.postedBy._id === state._id && <i className="material-icons" style={{ float: "right" }} onClick={() => deletePost(item._id)}>delete</i>}</h5>
                         <div className="card-image">
                             <img src={item.photo} />
                         </div>
--- server/routes/post.js
+++ server/routes/post.js
@@ -106,23 +106,20 @@ router.put('/comment', requireLogin, (req, res) => {
         });
 });
 
-router.delete('/deletepost/:postId', requireLogin, (req, res) => {
-    Post.findOne({ _id: req.params.postId })
-        .populate("postedBy", "_id")
-        .exec((err, post) => {
-            if (err || !post) {
-                return res.status(422).json({ error: err });
-            }
-            if (post.postedBy._id.toString() === req.user._id.toString()) {
-                post.remove()
-                    .then(result => {
-                        res.json({ message: "Successfully deleted the post" });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            }
-        });
+router.delete('/deletepost/:postId', requireLogin, async (req, res) => {
+    try {
+        const post = await Post.findOne({ _id: req.params.postId }).populate("postedBy", "_id");
+        if (!post) {
+            return res.status(422).json({ error: "Post not found" });
+        }
+        if (post.postedBy._id.toString() === req.user._id.toString()) {
+            const result = await Post.deleteOne({ _id: req.params.postId });
+            res.json(result);
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(422).json({ error: err });
+    }
 });
 
 
