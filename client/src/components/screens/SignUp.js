f653d870ce6496548f372d802d0bc120d41d2f49
dimanranawaka <dimanranawaka@gmail.com>
2024-06-13 19:10:39 +0530

    implement upload profile pic at signup

--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -7,8 +7,29 @@ const SignUp = () => {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [image, setImage] = useState("");
+    const [url, setUrl] = useState("");
 
-    const PostData = () => {
+    const uploadPic = () => {
+        const data = new FormData();
+        data.append("file", image);
+        data.append("upload_preset", "insta-clone");
+        data.append("cloud_name", "dyeeglaz1");
+        fetch("https://api.cloudinary.com/v1_1/dyeeglaz1/image/upload", {
+            method: "post",
+            body: data
+        })
+            .then(res => res.json())
+            .then(data => {
+                setUrl(data.url);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    const uploadFields = () => {
+
         if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
             M.toast({ html: "Invalid email", classes: "#c62828 red darken-3" });
             return;
@@ -34,6 +55,18 @@ const SignUp = () => {
             }).catch(err => {
                 console.log(err);
             });
+
+    }
+
+    const PostData = () => {
+
+        if (image) {
+            uploadPic();
+        }
+        else {
+            uploadFields();
+        }
+
     }
 
     return (
@@ -43,6 +76,16 @@ const SignUp = () => {
                 <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
                 <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                <div className="file-field input-field">
+                    <div className="btn">
+                        <span>Upload Profile Pic</span>
+                        <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+                    </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
+                    </div>
+                </div>
 
                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" onClick={() => PostData()}>
                     Sign Up
